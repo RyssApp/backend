85568cc92a9823f883a65293d00b84d70e61f72e
Justus K <justus.k@protonmail.com>
2020-03-21 17:40:43 +0100

    Fix all issues in session service

--- src/go/session-service/delivery/grpc.go
+++ src/go/session-service/delivery/grpc.go
@@ -26,6 +26,11 @@ func NewServer(repo session.Repository, secret string, expiration time.Duration)
 }
 
 func (s *SessionServiceServer) Create(ctx context.Context, req *pb.CreateSessionRequest) (*pb.CreateSessionResponse, error) {
+	val, _ := s.repo.GetToken(req.GetUserId())
+	if val != "" {
+		return &pb.CreateSessionResponse{Token: val}, nil
+	}
+
 	claims := &jwt.StandardClaims{
 		ExpiresAt: time.Now().Add(s.expiration).Unix(),
 		Subject:   req.GetUserId(),
@@ -52,35 +57,6 @@ func (s *SessionServiceServer) Verify(ctx context.Context, req *pb.VerifySession
 	return val, err.Err()
 }
 
-func validateToken(req *pb.VerifySessionRequest, secret string) (*pb.VerifySessionResponse, *status.Status) {
-	token, err := jwt.ParseWithClaims(req.GetToken(), &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, status.Error(codes.InvalidArgument, "Expected HMAC signing method.")
-		}
-		return []byte(secret), nil
-	})
-
-	if token.Valid {
-		if token.Claims.(*jwt.StandardClaims).Subject == req.GetUserId() {
-			return &pb.VerifySessionResponse{}, nil
-		} else {
-			return nil, status.New(codes.InvalidArgument, "User Ids doesn't match.")
-		}
-	} else if ve, ok := err.(*jwt.ValidationError); ok {
-		if ve.Errors&jwt.ValidationErrorMalformed != 0 {
-			return nil, status.New(codes.InvalidArgument, "Invalid token provided")
-		} else if ve.Errors&(jwt.ValidationErrorExpired|jwt.ValidationErrorNotValidYet) != 0 {
-			return nil, status.New(codes.Unauthenticated, "Token is expired.")
-		} else {
-			zap.L().Warn("JWT token couldn't be parsed.", zap.Error(err))
-			return nil, status.New(codes.InvalidArgument, "JWT Token could not be parsed.")
-		}
-	} else {
-		zap.L().Warn("JWT token couldn't be parsed.", zap.Error(err))
-		return nil, status.New(codes.InvalidArgument, "JWT Token could not be parsed.")
-	}
-}
-
 func (s *SessionServiceServer) Delete(ctx context.Context, req *pb.DeleteSessionRequest) (*pb.DeleteSessionResponse, error) {
 	_, err := validateToken(&pb.VerifySessionRequest{Token: req.GetToken(), UserId: req.GetUserId()}, s.secret)
 	if err != nil && err.Code() != codes.Unauthenticated {
@@ -93,4 +69,37 @@ func (s *SessionServiceServer) Delete(ctx context.Context, req *pb.DeleteSession
 		return nil, status.Error(codes.Internal, "Internal server error. Token was not deleted.")
 	}
 	return &pb.DeleteSessionResponse{}, nil
+}
+
+func validateToken(req *pb.VerifySessionRequest, secret string) (*pb.VerifySessionResponse, *status.Status) {
+	token, err := jwt.ParseWithClaims(req.GetToken(), &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, status.Error(codes.InvalidArgument, "Expected HMAC signing method.")
+		}
+		return []byte(secret), nil
+	})
+
+	if token != nil && token.Valid {
+		if token.Claims.(*jwt.StandardClaims).Subject == req.GetUserId() {
+			return &pb.VerifySessionResponse{}, nil
+		} else {
+			return nil, status.New(codes.InvalidArgument, "User Ids doesn't match.")
+		}
+	} else if ve, ok := err.(*jwt.ValidationError); ok {
+		if ve.Errors&jwt.ValidationErrorMalformed != 0 {
+			return nil, status.New(codes.InvalidArgument, "Invalid token provided")
+		} else if ve.Errors&(jwt.ValidationErrorExpired|jwt.ValidationErrorNotValidYet) != 0 {
+			err := s.repo.DelToken(req.GetUserId())
+			if err != nil {
+				zap.L().Error("Failed to delete token", zap.Error(err))
+			}
+			return nil, status.New(codes.Unauthenticated, "Token is expired.")
+		} else {
+			zap.L().Warn("JWT token couldn't be parsed.", zap.Error(err))
+			return nil, status.New(codes.InvalidArgument, "JWT Token could not be parsed.")
+		}
+	} else {
+		zap.L().Warn("JWT token couldn't be parsed.", zap.Error(err))
+		return nil, status.New(codes.InvalidArgument, "JWT Token could not be parsed.")
+	}
 }
